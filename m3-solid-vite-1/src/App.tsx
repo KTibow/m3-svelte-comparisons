dca1ecca03e33e09a2fcfaec68664c15f97326b5
KTibow <KTibow@users.noreply.github.com>
2026-01-23 20:10:23 -0800

    bench m3 solid

---
+++ m3-solid-vite-1/src/App.tsx
@@ -0,0 +1,10 @@
+// import { createSignal } from "solid-js";
+import "m3-solid";
+import "m3-solid/styles.css";
+import "m3-solid/themes/default.css";
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ m3-solid-vite-2/src/App.tsx
@@ -0,0 +1,11 @@
+// import { createSignal } from "solid-js";
+import { Button } from "m3-solid";
+import "m3-solid";
+import "m3-solid/styles.css";
+import "m3-solid/themes/default.css";
+
+function App() {
+  return <Button>Hi</Button>;
+}
+
+export default App;
